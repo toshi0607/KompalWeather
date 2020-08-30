771aaead8d4d8d03754581cbf3568759c1ef9b13
toshi0607 <s.toshi0607@gmail.com>
2020-08-30 11:21:24 +0900

    :bug: Fix format errors

--- pkg/message/builder.go
+++ pkg/message/builder.go
@@ -18,21 +18,21 @@ func Build(r *analyzer.Result) string {
 	m.WriteString(maleMessage(r))
 	m.WriteString(femaleMessage(r))
 	m.WriteString(fmt.Sprintf("（%s現在）\n", r.LatestStatus.Timestamp.Format("2006年01月02日 15時04分")))
-	m.WriteString(fmt.Sprint("最新状況はHPから！ https://kom-pal.com/"))
+	m.WriteString("最新状況はHPから！ https://kom-pal.com/")
 
 	return m.String()
 }
 
 func maleMessage(r *analyzer.Result) string {
 	if r.MaleTrend == analyzer.Unknown {
-		return fmt.Sprintf("男湯サウナは現在確認中です。\n")
+		return "男湯サウナは現在確認中です。\n"
 	}
 	return fmt.Sprintf("男湯サウナは%s現在%s\n", r.MaleTrend, r.LatestStatus.MaleSauna)
 }
 
 func femaleMessage(r *analyzer.Result) string {
 	if r.FemaleTrend == analyzer.Unknown {
-		return fmt.Sprintf("女湯サウナは現在確認中です。\n")
+		return "女湯サウナは現在確認中です。\n"
 	}
 	return fmt.Sprintf("女湯サウナは%s現在%s\n", r.FemaleTrend, r.LatestStatus.FemaleSauna)
 }
